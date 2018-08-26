bae4c3e56be0bb7dd672a80ac8183f39ded6de9c
ratnangshu <34093358+ratnangshu@users.noreply.github.com>
2018-08-26 20:10:57 +0530

    Add files via upload

---
+++ MPU6050_accel_pitch_roll/workshop/MPU6050_accel_pitch_roll/MPU6050_accel_pitch_roll.ino
@@ -0,0 +1,100 @@
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+int in1=7;
+int in2=8;
+int in4=9;
+int in3=10;
+int en1=11;
+int en2=12;
+//int inte=2;
+void setup() 
+{
+  Serial.begin(115200);
+
+  Serial.println("Initialize MPU6050");
+
+  while(!mpu.begin(MPU6050_SCALE_1000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+//  pinMode(inte,INPUT_PULLUP);
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+  pinMode(en1,OUTPUT);
+  pinMode(en2,OUTPUT);
+
+//  attachInterrupt(digitalPinToInterrupt(inte), loop , CHANGE);
+}
+
+void loop()
+{
+  // Read normalized values 
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  // Calculate Pitch & Roll
+  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
+  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis)*180.0)/M_PI;
+
+  // Output
+  Serial.print(" Pitch = ");
+  Serial.print(pitch);
+  Serial.print(" Roll = ");
+  Serial.print(roll);
+  
+  Serial.println("\n");
+  if(pitch>-10 && pitch<10 && roll>-20 && roll<230)
+  {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  analogWrite(en1,255);
+  analogWrite(en2,255);
+  }
+  else if(pitch<-10)
+  {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(en1,255);
+  analogWrite(en2,255);
+  }
+  else if(pitch>10)
+  {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(en1,255);
+  analogWrite(en2,255);
+  }
+  else if(roll>20)
+  {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(en1,255);
+  analogWrite(en2,255);
+  }
+  else if(roll<-20)
+  {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(en1,255);
+  analogWrite(en2,255);
+  }
+  delay(100);
+//  Serial.print("ABCD ");
+}
+
+
